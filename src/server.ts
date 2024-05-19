af8c5ea60e9e1427f4ea15de1f568e1dd57346e0
Daniel Wait <daniel.wait@lifeq.com>
2024-05-19 22:15:47 +0200

    Project setup and HTTP server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { Request, Response, Application } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
