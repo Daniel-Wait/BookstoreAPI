992f62088aacd58836055d1797b2de10d076287a
Daniel Wait <daniel.wait@lifeq.com>
2024-05-20 00:15:28 +0200

    Add logic checks for ids

--- src/n_layered/business_logic.ts
+++ src/n_layered/business_logic.ts
@@ -11,7 +11,11 @@ export class BusinessLogicLayer{
 
     // CRUD
     create(book : Book){
-       this.data_access.create(book);
+        const existingBook = this.data_access.read().find(b => b.id === book.id);
+        if (existingBook) {
+            throw new Error(`Book with ID ${book.id} already exists.`);
+        }
+        this.data_access.create(book);
     }
 
     read() : Book[] {
@@ -19,10 +23,18 @@ export class BusinessLogicLayer{
     }
 
     update(book : Book) {
+        const existingBook = this.data_access.read().find(b => b.id === book.id);
+        if (!existingBook) {
+            throw new Error(`Book with ID ${book.id} does not exist.`);
+        }
         this.data_access.update(book);
     }
 
     delete(id : number) {
+        const existingBook = this.data_access.read().find(b => b.id === id);
+        if (!existingBook) {
+            throw new Error(`Book with ID ${id} does not exist.`);
+        }
         this.data_access.delete(id);
     }
 }
